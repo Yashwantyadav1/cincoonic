0f237c4a060d7e449c8bf168ffe43ac33b41f46b
Yashwantyadav1 <yy888275@gmail.com>
2025-02-10 14:59:37 +0530

    loops

--- 3rd _day/03_while_loop.py
+++ 3rd _day/03_while_loop.py
@@ -10,9 +10,16 @@ while i<8:
      break
     i +=1 '''
     
-i=1
+'''i=1
 while i<9:
     print(i)
     if i==5:
         break
+    i +=1'''
+
+i=0
+while i<9:
     i +=1
+    if i==5:
+        continue
+    print(i)
