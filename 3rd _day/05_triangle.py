2b6edbdeced301ecf2d2afb099046ba35bc4ab14
Yashwantyadav1 <yy888275@gmail.com>
2025-02-10 16:47:59 +0530

    triangle

---
+++ 3rd _day/05_triangle.py
@@ -0,0 +1,13 @@
+
+n=5
+for x in range(1,n+1):
+    for i in range (n-x) :
+        print("  ", end="")
+    for j in range (1,(2*x)) :
+        print("*", end=" ")
+    print()
+            
+            
+            
+            
+            
