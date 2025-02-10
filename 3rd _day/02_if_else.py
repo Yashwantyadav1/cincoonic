8846935afb6d4c8c2008e0a4a45578c2c835c53a
Yashwantyadav1 <yy888275@gmail.com>
2025-02-10 14:52:12 +0530

    loops

---
+++ 3rd _day/02_if_else.py
@@ -0,0 +1,16 @@
+a=3
+b=3
+if(a<b):
+    print("a is greater number " )
+    
+elif(a>b):
+    print("the value of a is equall to the value of b")
+else:
+    print("both is greater nnumber")
+# AND statement practice 
+d=5
+e=7
+f=9
+
+if d==e or d<e :
+    print("At least one of the condition is true ")      
---
+++ 3rd _day/03_while_loop.py
@@ -0,0 +1,18 @@
+'''i=0
+while i<6 : 
+    print(i)
+    i +=1'''
+
+'''i=0 
+while i<8:
+    print(i)
+    if i==5:
+     break
+    i +=1 '''
+    
+i=1
+while i<9:
+    print(i)
+    if i==5:
+        break
+    i +=1
