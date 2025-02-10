78e183ccb7c50281af7281150cba27a15dde1b60
Yashwantyadav1 <yy888275@gmail.com>
2025-02-10 15:47:13 +0530

    loops

---
+++ 3rd _day/04_for_loop.py
@@ -0,0 +1,30 @@
+'''fruite = ["apple","banana","grapes" ,"etc"]
+for x in fruite :
+ print(x)'''
+ 
+'''fruite = ["apple","banaya","grapes","carrot" ,"etc"]
+for x in fruite:
+   if x== "apple" :
+        continue
+print(x)'''
+
+'''fruite = ["apple" ,"grapes","banana","carrot" , "etc"]
+for y in fruite :
+    if y == "carrot":
+        break
+    print(y)'''
+    
+'''for x in range (6):
+    print(x)'''
+    
+'''for x in range(2,9):
+    print(x)'''
+    
+''''for x in range(2, 39, 4):
+    print(x)'''
+    
+for x in range(5,30,4):
+    print(x)
+else:
+    print ("finallisttic" )
+    
