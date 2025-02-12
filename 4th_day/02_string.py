df783e8650208b56f5d141c4c66d756a054f0c5a
Yashwantyadav1 <yy888275@gmail.com>
2025-02-12 14:28:15 +0530

    string

---
+++ 4th_day/02_string.py
@@ -0,0 +1,13 @@
+a= "hello world "
+print(a) # assign string to a variable 
+
+print(" I done my given tasks")
+
+b = "hey how are you " # In this code it show the slice character.
+
+first_char = b[2]
+print(first_char)
+
+slice_b = b[0:7]
+print(slice_b)
+
